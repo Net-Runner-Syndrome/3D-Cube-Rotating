54a095cc063e71c0496df07282eb196a52c93e52
Zypher <zypherluszola@gmail.com>
2025-09-14 20:30:54 +0200

    [+] commit 1:

---
+++ code/main.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+// 1. Scene 
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#000000');
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
+
+// 3. Create and add Object
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+// 4. lighting 
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1,1,1);
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// 6. Animation
+function animate(){
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop( animate );
